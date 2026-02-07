e2f016ebee3c5cdd8bc0bbf8a31048ee7c0f07f7
yosefnuragaw <yosefnuragaw@gmail.com>
2026-02-07 18:45:57 +0700

    chore: change config

--- eval.py
+++ eval.py
@@ -135,7 +135,6 @@ def eval(model, loader: DataLoader, multiplier: float, layers: List[int], epoch:
         for input_ids, attention_mask in zip(batch["input_ids"], batch["attention_mask"]):
             
             input_ids = input_ids.to(model.device).squeeze(0)
-            logging.info(f"INPU SHAPE: {input_ids.shape}")
             attention_mask = attention_mask.to(model.device).squeeze(0)
     
             with torch.no_grad():
@@ -144,9 +143,6 @@ def eval(model, loader: DataLoader, multiplier: float, layers: List[int], epoch:
                 
                 sliced = logps[0, q_len - 1:]
                 avg_logp.append(sliced.mean().item())
-                logging.info(f"INPU SHAPE: {sliced.shape}")
-                logging.info(f"INPU SHAPE: {avg_logp}")
-
 
         pred = OPT[avg_logp.index(max(avg_logp))]
 
